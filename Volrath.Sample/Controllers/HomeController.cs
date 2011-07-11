d2d62b11e44ce5a9bda64e6bc0bf88fbc6c8d055
Felipe Oliveira <felipe.oliveira@abril.com.br>
2011-07-11 15:30:06 -0300

    formatting file

--- Volrath.Sample/Controllers/HomeController.cs
+++ Volrath.Sample/Controllers/HomeController.cs
@@ -5,26 +5,42 @@ using System.Web;
 using System.Web.Mvc;
 
 namespace Volrath.Sample.Controllers {
-  public class HomeController : Controller {
-    //
-    // GET: /Home/
+    public class HomeController : Controller {
+        //
+        // GET: /Home/
 
-    public ActionResult Index() {
-      return View();
+        public ActionResult Index() {
+            var permissions = (from p in Permission.db
+                select p);
+            var rolePermissions = (from p in RolePermission.db
+                select p);
+            var role = (from p in Role.db
+                select p);
+            var userRole = (from p in UserRole.db
+                select p);
+            
+            var permissions2 = (from p in Permission.db
+                join rp in RolePermission.db on p.Id equals rp.PermissionId
+                /*join r in Role.db on rp.RoleId equals r.Id
+                join ur in UserRole.db on r.Id equals ur.RoleId
+                where ur.UserId == 1*/
+                select p).ToList();
+            
+            return View();
+        }
+
+        public string Show() {
+            var user = (from u in Volrath.User.db
+                select u).First();
+            
+            return "show";
+        }
+
+        public string List() {
+            var user = (from u in Volrath.User.db
+                select u).ToList();
+            
+            return "list";
+        }
     }
-
-    public string Show() {
-      var user = (from u in Volrath.User.db
-                  select u).First();
-
-      return "show";
-    }
-
-    public string List() {
-      var user = (from u in Volrath.User.db
-                  select u).ToList();
-
-      return "list";
-    }
-  }
 }
